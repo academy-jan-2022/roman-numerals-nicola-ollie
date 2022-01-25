ba04005f417ea56a15e62fea92b0bb23ebdd84d0
Nicola <nicola_rushton@outlook.com>
2022-01-25 10:06:55 +0000

    refactor

--- src/test/java/RomanNumeralShould.java
+++ src/test/java/RomanNumeralShould.java
@@ -1,81 +1,37 @@
 import example.RomanNumeral;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralShould {
-    @Test
-    public void
-    return_I_when_given_1(){
+    @ParameterizedTest
+    @MethodSource("numeralTestCase") public void
+    return_roman_numeral_when_given_arabic_number(String roman, int arabic) {
         var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(1);
-        assertEquals("I", result);
+        var result = romanNumeral.convert(arabic);
+        assertEquals(roman, result);
+
     }
 
-    @Test
-    public void
-    return_II_when_given_2(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(2);
-        assertEquals("II", result);
+    private static Stream<Arguments> numeralTestCase() {
+        return Stream.of(
+                Arguments.of("I", 1),
+                Arguments.of("II", 2),
+                Arguments.of("III", 3),
+                Arguments.of("V", 5),
+                Arguments.of("VI", 6),
+                Arguments.of("VII", 7),
+                Arguments.of("VIII", 8),
+                Arguments.of("X", 10),
+                Arguments.of("XX", 20),
+                Arguments.of("XX", 20),
+                Arguments.of("XXVI", 26)
+        );
     }
-
-    @Test
-    public void
-    return_III_when_given_3(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(3);
-        assertEquals("III", result);
-    }
-
-    @Test
-    public void
-    return_V_when_given_5(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(5);
-        assertEquals("V", result);
-    }
-
-    @Test
-    public void
-    return_VI_when_given_6(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(6);
-        assertEquals("VI", result);
-    }
-
-    @Test
-    public void
-    return_VII_when_given_7(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(7);
-        assertEquals("VII", result);
-    }
-
-    @Test
-    public void
-    return_VIII_when_given_8(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(8);
-        assertEquals("VIII", result);
-    }
-
-    @Test
-    public void
-    return_X_when_given_10(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(10);
-        assertEquals("X", result);
-    }
-
-    @Test
-    public void
-    return_XX_when_given_20(){
-        var romanNumeral = new RomanNumeral();
-        var result = romanNumeral.convert(20);
-        assertEquals("XX", result);
-    }
-
-
 
 }
