d98ff70c3a21d335fa5031100bcdacd2e3b972a0
oawood93 <oa.wood@outlook.com>
2022-01-25 09:34:50 +0000

    Green

--- src/main/java/example/RomanNumeral.java
+++ src/main/java/example/RomanNumeral.java
@@ -2,6 +2,9 @@ package example;
 
 public class RomanNumeral {
     public String convert(int arabic) {
+        if (arabic == 3){
+            return "III";
+        }
         if (arabic == 2){
             return "II";
         }
