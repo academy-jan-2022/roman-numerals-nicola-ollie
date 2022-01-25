9ebb4aabb22510d0610912f3208d828d315576bc
Nicola <nicola_rushton@outlook.com>
2022-01-25 09:47:02 +0000

    refactor

--- src/main/java/example/RomanNumeral.java
+++ src/main/java/example/RomanNumeral.java
@@ -2,19 +2,13 @@ package example;
 
 public class RomanNumeral {
     public String convert(int arabic) {
-        if (arabic == 5) {
-            return "V";
-        }
-
-        if (arabic == 6){
-            return "VI";
-        }
-
-        if (arabic == 7){
-            return "VII";
-        }
-
         var numeralString = "";
+
+        if (arabic >= 5){
+            numeralString += "V";
+            arabic -= 5;
+        }
+
         for (var i = 0; i < arabic; i++){
             numeralString += "I";
         }
